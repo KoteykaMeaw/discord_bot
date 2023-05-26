27e8c281f3e49022bf37f153e6bf6a8aca314519
Koteyka <131580914+KoteykaMeaw@users.noreply.github.com>
2023-05-26 19:08:40 +0700

    Update discord_bruh.py

--- discord_bruh.py
+++ discord_bruh.py
@@ -15,34 +15,11 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content.startswith('привет'):
-        await message.channel.send("привет!")
-    if message.content.startswith('как дела?'):
-        await message.channel.send("норм") 
-    if message.content.startswith('тоже'):
-        await message.channel.send("ок")
-    if message.content.startswith('вав'):
-        await message.channel.send("ваув")
-    elif message.content.startswith('пока'):
-        await message.channel.send("пока(")
-    elif message.content.startswith('го в рб'):
-        await message.channel.send("го")
-    elif message.content.startswith('пон'):
-        await message.channel.send("в жопе лимон")
-    elif message.content.startswith('пароль'):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith('смайлик'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('монетка'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('игра'):
-        await message.channel.send(gameee())
-    elif message.content.startswith('!help'):
-        await message.channel.send('на некоторые сообщения этот бот отвечает,а некоторые повторяет. есть пару компнд - смайлик , монетка , игра , пароль и команда !help.')
-    else:
-        await message.channel.send(message.content)
-bot.run('MTExMDU1Nzk1ODk4ODg5ODM0NA.Gum7DP.UgoatLXkHOkMYnZwzlaGCqAua1JqADW8_t2shU')
+async def on_member_join(member):
+    print(f'{member} зашёл на сервер.')
+    await member.send(f"Привет {member}!")
+
+bot.run('token here')
+
